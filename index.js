6fe6ac2c1993e7840051adf196a59efd3ed89c6b
sarpisik <contact@sarpisik.com>
2019-05-21 16:49:04 +0200

    Add repeat calculation

--- index.js
+++ index.js
@@ -73,13 +73,13 @@ const container = document.getElementById('main');
 
 class Calculator {
   constructor() {
-    this.memory;
     this.input = '';
     this.operations = {
       '+': this.getSum,
       '-': this.getSubtraction,
       '÷': this.getDivision,
-      x: this.getMultiply
+      x: this.getMultiply,
+      '=': this.getResult
     };
     this.inputScreen = new InputScreen();
     this.outputScreen = new OutputScreen();
@@ -89,30 +89,62 @@ class Calculator {
     container.appendChild(this.inputScreen.element);
     container.appendChild(this.outputScreen.element);
   }
-  handleOperation(nextAction) {
-    if (this.memory === undefined) {
-      this.finishOperation(this.input, nextAction);
-    } else {
-      const operator = this.operations[this.memory.action].bind(this);
-      const result = operator(Number(this.input));
-      this.finishOperation(result, nextAction);
-    }
-  }
-  finishOperation(result, nextAction) {
-    this.updateMemory(result, nextAction);
+  updateDom(result) {
     this.updateOutputScreen(result);
     this.resetInput();
+  }
+  handleOperation(action) {
+    const inputNumber = parseFloat(this.input);
+    if (!this.memory) {
+      this.createMemory(inputNumber || 0, action);
+      this.updateDom(inputNumber || 0);
+    } else if (!this.memory.action) {
+      if (action === '=') {
+        const result = this.handleCalculate(
+          this.memory.lastAction,
+          inputNumber || this.memory.lastInput
+        );
+        this.finishOperation(result, null);
+      } else {
+        this.memory.lastAction = null;
+        this.memory.action = action;
+      }
+    } else {
+      const result = this.handleCalculate(
+        this.memory.action,
+        inputNumber || this.memory.data
+      );
+      if (action === '=') {
+        this.updateMemory('lastAction', this.memory.action);
+        this.updateMemory('lastInput', inputNumber);
+        this.finishOperation(result, null);
+        return;
+      }
+
+      this.finishOperation(result, action);
+    }
+  }
+  createMemory(data, action) {
+    this.memory = {
+      data,
+      action
+    };
+  }
+  handleCalculate(action, secondValue) {
+    const operator = this.operations[action].bind(this);
+    return operator(secondValue);
+  }
+  finishOperation(result, nextAction) {
+    this.updateMemory('data', result);
+    this.updateMemory('action', nextAction);
+    this.updateDom(result);
   }
   addInput(input) {
     this.input += input;
     this.inputScreen.updateScreen(this.input);
   }
-  updateMemory(input, action) {
-    const data = Number(input);
-    this.memory = {
-      data,
-      action
-    };
+  updateMemory(property, value) {
+    this.memory[property] = value;
   }
   updateOutputScreen(data) {
     this.outputScreen.updateScreen(data);
@@ -133,6 +165,9 @@ class Calculator {
   getMultiply(data) {
     return this.memory.data * data;
   }
+  getResult(data) {
+    return data;
+  }
 }
 const calculator = new Calculator();
 const newEls = [
@@ -142,7 +177,8 @@ const newEls = [
   new OperatorButton('+'),
   new OperatorButton('-'),
   new OperatorButton('÷'),
-  new OperatorButton('x')
+  new OperatorButton('x'),
+  new OperatorButton('=')
 ];
 newEls.forEach(function(el, i) {
   container.appendChild(el);
